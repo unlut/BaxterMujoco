8c25677b6b7b93175b70e8a782431451557117d3
TolgaOk <tolgaokk@gmail.com>
2018-09-21 15:07:19 +0300

    Unfinished version!

--- master_actuator.py
+++ master_actuator.py
@@ -3,6 +3,7 @@ import numpy as np
 import inspect
 import time
 import matplotlib.pyplot as plt
+import inspect
 
 fullpath = "/home/tolga/neural_ws/baxter_RL/baxter/master.xml"
 model = mujoco_py.load_model_from_path(fullpath)
@@ -15,24 +16,37 @@ step = 0
 
 # time.sleep(2)
 # exit()
+
+def useful_functions():
+    funcs = ("sim.data.get_body_xpos",
+             "sim.data.ctrl",
+             "sim.model.joint_name2id",
+             "sim.model.body_name2id")
+    return funcs
+
+    
+
+
 while True:
     sim.step()
-    if step == -1:
-        print(sim.model.actuator_trnid.shape)
-        print(sim.model.jnt_qposadr.shape)
-        print(sim.model.actuator_biastype.shape)
-        print(sim.data.ctrl)
-        print(sim.data.qpos)
-        break
+    if step == 3:
+        print(dir(sim.data))
+        # print(inspect.getsource(sim.data))
+        # print(sim.data.get_body_xpos("kase"))
+        print(sim.data.get_joint_qvel("left_e0"))
+        print(len(sim.data.ctrl))
+        print(sim.model.joint_name2id("left_e0"))
+        print(len(sim.model.joint_names))
+        #break
     step += 1
-    act = np.random.normal(size=18)
+    act = np.random.normal(size=len(sim.data.ctrl))
     # sim.data.ctrl[0] += -0.0001
     # sim.data.ctrl[1] -= 0.0006
     # sim.data.ctrl[2] -= 0.0001
     # sim.data.ctrl[3] += 0.0005
     sim.data.ctrl[:] += act/100
 
-    if step%3000 == -1:
+    if step%3000 == 1:
         # rgb, depth = viewer.read_pixels(120, 120, True)
         # plt.imshow(np.flip(rgb, 0), cmap="magma_r")
 
